cd0e567fc12bb0b204e8a4c4d0355c4b3e7e178c
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-06-16 16:05:16 +0800

    0.0.3

--- GET_INFO_FROM_URL.py
+++ GET_INFO_FROM_URL.py
@@ -1,35 +1,51 @@
 import argparse
 from urllib.parse import urlparse, parse_qs
 
+# 这个函数用于从URL中提取出参数
 def extract_parameters(url):
+    # 解析URL
     parsed_url = urlparse(url)
+    # 提取URL的参数部分
     params = parse_qs(parsed_url.query)
 
+    # 从URL的路径中获取app_token，路径的最后一部分即为app_token
     app_token = parsed_url.path.split('/')[-1]
+    # 从参数中获取table_id，如果没有table参数则返回None
     table_id = params.get('table', [None])[0]
+    # 从参数中获取view_id，如果没有view参数则返回None
     view_id = params.get('view', [None])[0]
 
+    # 返回提取出的app_token, table_id, view_id
     return app_token, table_id, view_id
 
+# 从URL中提取app_token
 def GET_APPTOKEN_FROM_URL(url):
     app_token, _, _ = extract_parameters(url)
     return app_token
 
+# 从URL中提取table_id
 def GET_TABLEID_FROM_URL(url):
     _, table_id, _ = extract_parameters(url)
     return table_id
 
+# 从URL中提取view_id
 def GET_VIEWID_FROM_URL(url):
     _, _, view_id = extract_parameters(url)
     return view_id
 
+# 主函数
 if __name__ == "__main__":
+    # 解析命令行参数
     parser = argparse.ArgumentParser()
+    # 添加一个名为url的参数，该参数为必需，其作用是提供用于提取参数的URL
     parser.add_argument('-u', '--url', required=True, help='用于提取参数的URL')
+    # 解析命令行参数
     args = parser.parse_args()
 
+    # 从URL中提取app_token, table_id, view_id
     app_token, table_id, view_id = extract_parameters(args.url)
 
+    # 打印提取出的app_token, table_id, view_id
     print(f"app_token: {app_token}")
     print(f"table_id: {table_id}")
     print(f"view_id: {view_id}")
--- LIST_DATATABLES.py
+++ LIST_DATATABLES.py
@@ -1,30 +1,51 @@
-import configparser
 import requests
+import configparser
 import json
+import argparse
 
-def read_config_file(file_path):
+
+# 封装的列表数据表的函数，接收四个可选参数
+def list_datatables(app_token=None, user_access_token=None, page_token=None, page_size=None):
+    # 读取配置文件
     config = configparser.ConfigParser()
-    config.read(file_path)
-    user_access_token = config.get('TOKEN', 'user_access_token')
-    app_token = config.get('TOKEN', 'app_token')
-    return user_access_token, app_token
+    config.read('feishu-config.ini')
 
-def list_tables(user_access_token, app_token):
-    url = f"https://open.feishu.cn/open-apis/bitable/v1/apps/{app_token}/tables?page_size=20"
+    # 如果参数为空，则使用配置文件中的默认值
+    if not app_token:
+        app_token = config.get('TOKEN', 'app_token')
+    if not user_access_token:
+        user_access_token = config.get('TOKEN', 'user_access_token')
+    if not page_token:
+        page_token = config.get('LIST_DATATABLES', 'page_token', fallback=None)
+    if not page_size:
+        page_size = config.get('LIST_DATATABLES', 'page_size', fallback=20)
+
+    # 构造请求URL和头部
+    url = f"https://open.feishu.cn/open-apis/bitable/v1/apps/{app_token}/tables"
     headers = {
-      'Authorization': f'Bearer {user_access_token}'
+        'Authorization': f'Bearer {user_access_token}',
+        'Content-Type': 'application/json; charset=utf-8'
     }
-    response = requests.request("GET", url, headers=headers)
-    return response.text
+
+    # 如果存在page_token，则添加到请求参数中
+    params = {'page_size': page_size}
+    if page_token:
+        params['page_token'] = page_token
+
+    # 发起请求，并返回响应体的json形式
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()
+
 
 if __name__ == "__main__":
-    user_access_token, app_token = read_config_file('feishu-config.ini')
-    response_text = list_tables(user_access_token, app_token)
+    # 解析命令行参数
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--app_token', help='app token')
+    parser.add_argument('--user_access_token', help='user access token')
+    parser.add_argument('--page_token', help='page token')
+    parser.add_argument('--page_size', type=int, help='page size')
+    args = parser.parse_args()
 
-    print(response_text)  # Print the original response
-
-    response_data = json.loads(response_text)
-
-    # Print table name and ID for each table
-    for table in response_data['data']['items']:
-        print(f"Table Name: {table['name']}, Table ID: {table['table_id']}")
+    # 调用封装的函数，使用命令行参数或默认值
+    response_body = list_datatables(args.app_token, args.user_access_token, args.page_token, args.page_size)
+    print(json.dumps(response_body, indent=4))

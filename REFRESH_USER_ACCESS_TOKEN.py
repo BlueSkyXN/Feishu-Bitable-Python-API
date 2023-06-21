cf94f964bc98a057bf523562dca920cc5dd68ce9
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-06-21 15:36:45 +0800

    1.20.4

--- REFRESH_USER_ACCESS_TOKEN.py
+++ REFRESH_USER_ACCESS_TOKEN.py
@@ -1,63 +1,78 @@
 import requests
-import json
 import configparser
+import json
+import os
 import argparse
-import codecs
 
-def REFRESH_USER_ACCESS_TOKEN(token=None, refresh=None):
+def REFRESH_USER_ACCESS_TOKEN(app_access_token=None, refresh_token=None):
+    # 配置文件路径
+    config_path='feishu-config.ini'
+
+    # 确保配置文件存在
+    if not os.path.exists(config_path):
+        with open(config_path, 'w') as f:
+            pass
+
+    # 读取配置文件
     config = configparser.ConfigParser()
-    with codecs.open('feishu-config.ini', 'r', encoding='utf-8') as f:
-        config.read_file(f)
+    config.read(config_path, encoding='utf-8')
 
+    # 确保 TOKEN 部分存在
     if not config.has_section('TOKEN'):
         config.add_section('TOKEN')
 
-    if 'app_access_token' not in config['TOKEN']: # Use 'app_access_token' instead of 'app_token'
-        config.set('TOKEN', 'app_access_token', '')
-        
-    if 'user_access_token' not in config['TOKEN']:
-        config.set('TOKEN', 'user_access_token', '')
+    # 从配置文件或者参数获取令牌
+    app_access_token = app_access_token or config.get('TOKEN', 'app_access_token', fallback=None)
+    refresh_token = refresh_token or config.get('TOKEN', 'refresh_token', fallback=None)
 
-    with codecs.open('feishu-config.ini', 'w', encoding='utf-8') as configfile:
-        config.write(configfile)
+    # 如果任一令牌不存在，需要用户先获取它
+    if not app_access_token or not refresh_token:
+        raise ValueError("Both app_access_token and refresh_token are required. Please get them first.")
 
-    token = config.get('TOKEN', 'app_access_token') if token is None else token # Use 'app_access_token' instead of 'app_token'
-    print(f"Using token: {token}")
-    refresh = config.get('TOKEN', 'user_access_token') if refresh is None else refresh
-    print(f"Using refresh token: {refresh}")
-
+    # 构建请求URL和请求头
     url = "https://open.feishu.cn/open-apis/authen/v1/refresh_access_token"
-    headers = {"Content-Type": "application/json"}
-
-    payload = {
-        "app_access_token": token,
-        "grant_type": "refresh_token",
-        "refresh_token": refresh
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8',
     }
 
+    # 构建请求体
+    payload = {
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token,
+        "app_access_token": app_access_token
+    }
+
+    # 发起请求
     response = requests.post(url, headers=headers, data=json.dumps(payload))
-    data = response.json()
-    if data["code"] != 0:
-        print(f"Error: {data['msg']}")
+    response_json = response.json()
+
+    # 检查响应状态码
+    if response.status_code != 200:
+        print(f"HTTP Error: {response_json}")
         return None
 
-    user_access_token = data['data']['user_access_token']
-    print(f"Refreshed User Access Token: {user_access_token}")
-    
-    config.set('TOKEN', 'user_access_token', user_access_token)
-    with codecs.open('feishu-config.ini', 'w', encoding='utf-8') as configfile:
-        config.write(configfile)
+    # 检查响应体中的 code
+    if response_json.get('code') != 0:
+        print(f"Response Error: {response_json}")
+        return None
 
-    return user_access_token
+    # 更新配置文件
+    if 'data' in response_json and 'access_token' in response_json['data']:
+        config.set('TOKEN', 'user_access_token', response_json['data']['access_token'])
+        config.set('TOKEN', 'refresh_token', response_json['data']['refresh_token'])
+        with open(config_path, 'w', encoding='utf-8') as configfile:
+            config.write(configfile)
+
+    return response_json.get('data', {}).get('access_token')
 
 def REFRESH_USER_ACCESS_TOKEN_CMD():
     parser = argparse.ArgumentParser(description='Refresh user access token.')
-    parser.add_argument('-t', '--token', type=str, help='The token.')
+    parser.add_argument('-a', '--app_token', type=str, help='The app access token.')
     parser.add_argument('-r', '--refresh', type=str, help='The refresh token.')
     args = parser.parse_args()
 
-    user_access_token = REFRESH_USER_ACCESS_TOKEN(args.token, args.refresh)
+    user_access_token = REFRESH_USER_ACCESS_TOKEN(args.app_token, args.refresh)
     print(f'User Access Token: {user_access_token}')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     REFRESH_USER_ACCESS_TOKEN_CMD()

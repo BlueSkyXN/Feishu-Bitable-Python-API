53db691283a87bc51435b5ef297871b60536568d
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-06-21 10:37:30 +0800

    1.17.2

--- REFRESH_USER_ACCESS_TOKEN.py
+++ REFRESH_USER_ACCESS_TOKEN.py
@@ -1,56 +1,58 @@
+import argparse
+import configparser
 import requests
 import json
-import configparser
-import argparse
+import codecs
 
-def REFRESH_USER_ACCESS_TOKEN(token, refresh_token):
-    url = 'https://open.feishu.cn/open-apis/authen/v1/refresh_access_token'
-    headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json; charset=utf-8'
-    }
+def REFRESH_USER_ACCESS_TOKEN(token=None, refresh=None):
+    config = configparser.ConfigParser()
+    with codecs.open('feishu-config.ini', 'r', encoding='utf-8') as f:
+        config.read_file(f)
+
+    if not config.has_section('TOKEN'):
+        config.add_section('TOKEN')
+        
+    if 'app_token' not in config['TOKEN']:
+        config.set('TOKEN', 'app_token', '')
+        
+    if 'user_access_token' not in config['TOKEN']:
+        config.set('TOKEN', 'user_access_token', '')
+
+    with codecs.open('feishu-config.ini', 'w', encoding='utf-8') as configfile:
+        config.write(configfile)
+
+    token = config.get('TOKEN', 'app_token') if token is None else token
+    refresh = config.get('TOKEN', 'user_access_token') if refresh is None else refresh
+
+    url = "https://open.feishu.cn/open-apis/authen/v1/refresh_access_token"
+    headers = {"Content-Type": "application/json"}
+
     payload = {
-        'grant_type': 'refresh_token',
-        'refresh_token': refresh_token
+        "app_access_token": token,
+        "grant_type": "refresh_token",
+        "refresh_token": refresh
     }
-    
+
     response = requests.post(url, headers=headers, data=json.dumps(payload))
-    if response.status_code == 200:
-        response_data = response.json()
-        if response_data.get('code') == 0:
-            user_access_token = response_data.get('data').get('access_token')
-            return user_access_token
-        else:
-            return None
-    else:
+    data = response.json()
+    if data["code"] != 0:
+        print(f"Error: {data['msg']}")
         return None
 
-def REFRESH_USER_ACCESS_TOKEN_CMD():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--token", required=True, help="App Access Token")
-    parser.add_argument("-r", "--refresh", required=True, help="Refresh Token")
-    args = parser.parse_args()
-    
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    
-    if not config.has_option('TOKEN', 'user_access_token'):
-        config.set('TOKEN', 'user_access_token', '')
-        with open('config.ini', 'w') as f:
-            config.write(f)
+    config.set('TOKEN', 'user_access_token', data['data']['user_access_token'])
+    with codecs.open('feishu-config.ini', 'w', encoding='utf-8') as configfile:
+        config.write(configfile)
 
-    if not config.has_option('TOKEN', 'refresh_token'):
-        config.set('TOKEN', 'refresh_token', config.get('TOKEN', 'user_access_token'))
-        with open('config.ini', 'w') as f:
-            config.write(f)
+    return data['data']['user_access_token']
+
+def REFRESH_USER_ACCESS_TOKEN_CMD():
+    parser = argparse.ArgumentParser(description='Refresh user access token.')
+    parser.add_argument('-t', '--token', type=str, help='The token.')
+    parser.add_argument('-r', '--refresh', type=str, help='The refresh token.')
+    args = parser.parse_args()
 
     user_access_token = REFRESH_USER_ACCESS_TOKEN(args.token, args.refresh)
-    if user_access_token:
-        config.set('TOKEN', 'user_access_token', user_access_token)
-        with open('config.ini', 'w') as f:
-            config.write(f)
-    else:
-        REFRESH_USER_ACCESS_TOKEN(args.token, config.get('TOKEN', 'user_access_token'))
+    print(f'User Access Token: {user_access_token}')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     REFRESH_USER_ACCESS_TOKEN_CMD()

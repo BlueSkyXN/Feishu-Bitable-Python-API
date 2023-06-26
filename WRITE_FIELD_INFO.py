98e69ad46ab5d8c2f6b4a177763261e1ee2dab4a
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-06-26 17:29:17 +0800

    2.2.1

--- WRITE_FIELD_INFO.py
+++ WRITE_FIELD_INFO.py
@@ -3,7 +3,7 @@ import argparse
 from GET_FIELD_INFO import GET_FIELD_INFO
 
 # 将字段信息写入配置文件
-def WRITE_FIELD_INFO(field_name=None, field_id=None, config_file=None):
+def WRITE_FIELD_INFO(field_name=None, field_id=None, app_token=None, table_id=None, view_id=None, page_token=None, page_size=None, config_file=None):
     if config_file is None:
         config_file = "feishu-config.ini"
 
@@ -11,7 +11,7 @@ def WRITE_FIELD_INFO(field_name=None, field_id=None, config_file=None):
     config.read(config_file, encoding="utf-8")
 
     # 调用GET_FIELD_INFO函数获取字段信息
-    field_info = GET_FIELD_INFO(field_name, field_id)
+    field_info = GET_FIELD_INFO(field_name, field_id, app_token, table_id, view_id, page_token, page_size, config_file)
 
     # 检查配置文件是否存在名为'FIELD_INFO'的section，如果不存在则添加
     if "FIELD_INFO" not in config:
@@ -36,11 +36,25 @@ def WRITE_FIELD_INFO_CMD():
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--name", default=None, help="字段名称")
     parser.add_argument("-i", "--id", default=None, help="字段ID")
+    parser.add_argument("--app_token", help="app token")
+    parser.add_argument("--table_id", help="table ID")
+    parser.add_argument("--view_id", help="view ID")
+    parser.add_argument("--page_token", help="page token")
+    parser.add_argument("--page_size", type=int, help="page size")
     parser.add_argument("--config_file", default="feishu-config.ini", help="配置文件路径")
     args = parser.parse_args()
 
     # 调用WRITE_FIELD_INFO函数，将字段信息写入配置文件
-    WRITE_FIELD_INFO(args.name, args.id, args.config_file)
+    WRITE_FIELD_INFO(
+        field_name=args.name,
+        field_id=args.id,
+        app_token=args.app_token,
+        table_id=args.table_id,
+        view_id=args.view_id,
+        page_token=args.page_token,
+        page_size=args.page_size,
+        config_file=args.config_file
+    )
 
 
 if __name__ == "__main__":

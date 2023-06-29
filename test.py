15c87aef61ed3fd38abe973bd402466830e36d40
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-06-29 10:50:54 +0800

    2.18.2

--- test.py
+++ test.py
@@ -4,11 +4,10 @@ from FeishuBitableAPI import FeishuBitableAPI
 api = FeishuBitableAPI()
 
 # 调用类的方法进行测试
-app_token = api.GET_APP_ACCESS_TOKEN(app_id="your_app_id", app_secret="your_app_secret", config_file="your_config_file.ini")
-table_info = api.GET_FIELD_INFO(field_name="your_field_name", app_token="your_app_token", table_id="your_table_id")
-# 其他方法的调用...
+app_token = api.GET_APPTOKEN_FROM_URL("https://ruijie.feishu.cn/base/VwGhbo65BaYLaOsSz1nciWc5ncb?table=tblzDsYuQkXfoy9e&view=vewAgGXuqf")
+
 
 # 打印测试结果或进行其他操作
 print("App Token:", app_token)
-print("Table Info:", table_info)
+
 # 其他结果的打印或操作...

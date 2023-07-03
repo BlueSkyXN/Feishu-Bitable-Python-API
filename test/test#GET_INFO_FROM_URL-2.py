c1c559f507ae72639db8c6198efe8a31c87548da
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-07-03 11:28:44 +0800

    3.0.3

---
+++ test/test#GET_INFO_FROM_URL-2.py
@@ -0,0 +1,17 @@
+#import sys
+#import os
+#sys.path.append(os.getcwd())
+
+from FeishuBitableAPI import FeishuBitableAPI
+
+# 创建 FeishuBitableAPI 类的实例
+api = FeishuBitableAPI()
+
+# 调用类的方法进行测试
+app_token = api.GET_APPTOKEN_FROM_URL("https://ruijie.feishu.cn/base/VwGhbo65BaYLaOsSz1nciWc5ncb?table=tblzDsYuQkXfoy9e&view=vewAgGXuqf")
+
+
+# 打印测试结果或进行其他操作
+print("App Token:", app_token)
+
+# 其他结果的打印或操作...

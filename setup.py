d74a9236143404ae4e2a656fd7bb1586647a63c2
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-07-06 17:16:23 +0800

    3.2.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FeishuBitableAPI',
-    version='3.2.5',
+    version='3.2.6',
     packages=find_packages(),
     #py_modules=[],
     url='https://github.com/BlueSkyXN/Feishu-Bitable-Python-API',
